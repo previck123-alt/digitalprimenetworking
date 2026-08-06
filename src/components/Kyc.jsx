109c397a84f4d87674eb5a4f8e80e536e2e81ff6
SkyNet <arierhiprecious@gmail.com>
2026-08-06 09:04:47 +0100

    latest

--- src/components/Kyc.jsx
+++ src/components/Kyc.jsx
@@ -9,30 +9,47 @@ import styles from './Kyc.module.css';
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 
-const KycWarningCard = ({title,message}) => {
+const KycWarningCard = ({
+  title = "",
+  message = "",
+}) => {
   const { user } = useSelector((state) => state.userAuth);
   const status = user?.kycVerified;
-  let navigate = useNavigate()
-
+  const navigate = useNavigate();
 
   let Icon = null;
   let cardStyle = styles.kycCard;
 
   const handleKycClick = () => {
-    // You can navigate or open a modal here
-    navigate('/registeration')
+    navigate("/registeration");
   };
 
   return (
     <div className={cardStyle}>
-      <div className={styles.iconWrapper}>
-        <Icon className={styles.icon} />
-      </div>
+      {Icon && (
+        <div className={styles.iconWrapper}>
+          <Icon className={styles.icon} />
+        </div>
+      )}
+
       <div className={styles.textWrapper}>
-        <h3 className={styles.title}>{title}</h3>
-        <p className={styles.message}>{message}</p>
-        {status === 'false' && (
-          <button onClick={handleKycClick} className={styles.kycButton}>
+        {title && (
+          <h3 className={styles.title}>
+            {title}
+          </h3>
+        )}
+
+        {message && (
+          <p className={styles.message}>
+            {message}
+          </p>
+        )}
+
+        {status === "false" && (
+          <button
+            onClick={handleKycClick}
+            className={styles.kycButton}
+          >
             Complete KYC Now
           </button>
         )}
